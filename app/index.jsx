cdebab7e9d1a47f9ff3bc04f20949b0f3089b4da
krishiiitp <98936053+krishiiitp@users.noreply.github.com>
2024-04-16 21:52:39 +0530

    creating landing page

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, Text, View,Image } from 'react-native';
+import { images } from "../constants"
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height : '100%'}}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain" 
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain" 
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
